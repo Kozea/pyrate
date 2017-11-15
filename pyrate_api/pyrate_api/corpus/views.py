713e86cae35444a1404265c677e752b6881e99c8
Sam <sam@samr1.net>
2017-11-15 15:15:20 +0100

    API: adding text in corpus (WIP)

--- pyrate_api/pyrate_api/corpus/views.py
+++ pyrate_api/pyrate_api/corpus/views.py
@@ -117,3 +117,56 @@ def get_corpus():
         }
     }
     return jsonify(response_object), 200
+
+
+@corpus_blueprint.route('/corpus', methods=['POST'])
+def add_corpus():
+    """Add a text in corpus"""
+    post_data = request.get_json()
+    if not post_data:
+        response_object = {
+            'status': 'fail',
+            'message': 'Invalid payload.'
+        }
+        return jsonify(response_object), 400
+    code = 401
+    auth_header = request.headers.get('Authorization')
+    if not auth_header:
+            response_object['message'] = 'Provide a valid auth token.'
+            code = 403
+            return jsonify(response_object), code
+    auth_token = auth_header.split(" ")[1]
+    user_id = User.decode_auth_token(auth_token)
+    if isinstance(user_id, str):
+        response_object['message'] = user_id
+        return jsonify(response_object), code
+
+    title = post_data.get('title')
+    filename = post_data.get('filename')
+    category_id = post_data.get('category_id')
+
+    try:
+        text = Corpus_text.query.filter_by(title=title).first()
+        if not text:
+            db.session.add(Corpus_category(title=title,
+                                           filename=filename,
+                                           category_id=category_id))
+            db.session.commit()
+            response_object = {
+                'status': 'success',
+                'message': f'"{label}" was added!'
+            }
+            return jsonify(response_object), 201
+        else:
+            response_object = {
+                'status': 'fail',
+                'message': 'Sorry. That category already exists.'
+            }
+            return jsonify(response_object), 400
+    except exc.IntegrityError as e:
+        db.session.rollback()
+        response_object = {
+            'status': 'fail',
+            'message': 'Invalid payload.'
+        }
+        return jsonify(response_object), 400
