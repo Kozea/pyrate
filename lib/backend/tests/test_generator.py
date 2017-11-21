76845a736e88fb560e5855e11dad8e4eb43a7b18
Sam <sam@samr1.net>
2017-11-21 14:05:56 +0100

    API: return generated text - tests refactor (WIP)

--- lib/backend/tests/test_generator.py
+++ lib/backend/tests/test_generator.py
@@ -1,5 +1,5 @@
 import json
-from ddt import ddt, data
+from ddt import ddt, data, unpack
 
 from base import BaseTestCase
 from utils import add_all, add_category, add_user
@@ -13,7 +13,7 @@ class TestGeneratorService(BaseTestCase):
 
     @data('markovify', 'MarkovChain')
     def test_train(self, value):
-        """=> Ensure model can be trained."""
+        """=> Ensure a model can be trained."""
         add_all()
 
         with self.client:
@@ -30,121 +30,6 @@ class TestGeneratorService(BaseTestCase):
             self.assertEqual(response.status_code, 200)
             self.assertIn('success', data['status'])
             self.assertIn(f'Model "{value}" trained. You can now generate texts.', data['message'])
-
-    def test_train_invalid_payload(self):
-        """=> Ensure error is thrown if no category and model are provided."""
-        add_all()
-
-        with self.client:
-            response = self.client.post(
-                '/train',
-                data=json.dumps(dict()),
-                content_type='application/json'
-            )
-            data = json.loads(response.data.decode())
-
-            self.assertEqual(response.status_code, 400)
-            self.assertIn('fail', data['status'])
-            self.assertIn('Invalid payload.', data['message'])
-
-    @data('markovify', 'MarkovChain')
-    def test_train_no_category(self, value):
-        """=> Ensure error is thrown if no category is provided."""
-        add_all()
-
-        with self.client:
-            response = self.client.post(
-                '/train',
-                data=json.dumps(dict(
-                    algo=value
-                    )),
-                content_type='application/json'
-            )
-            data = json.loads(response.data.decode())
-
-            self.assertEqual(response.status_code, 400)
-            self.assertIn('fail', data['status'])
-            self.assertIn('Invalid payload.', data['message'])
-
-    def test_train_no_algo(self):
-        """=> Ensure error is thrown if no model is provided."""
-        add_all()
-
-        with self.client:
-            response = self.client.post(
-                '/train',
-                data=json.dumps(dict(
-                    category_id=1
-                    )),
-                content_type='application/json'
-            )
-            data = json.loads(response.data.decode())
-
-            self.assertEqual(response.status_code, 400)
-            self.assertIn('fail', data['status'])
-            self.assertIn('Invalid payload.', data['message'])
-
-    def test_train_invalid_model(self):
-        """=> Ensure error is thrown if model does not exist"""
-        add_all()
-
-        with self.client:
-            response = self.client.post(
-                '/train',
-                data=json.dumps(dict(
-                    algo='no-model',
-                    category_id=1
-                    )),
-                content_type='application/json'
-            )
-            data = json.loads(response.data.decode())
-
-            self.assertEqual(response.status_code, 400)
-            self.assertIn('error', data['status'])
-            self.assertIn('Selected algorithm does not exist.', data['message'])
-
-    @data('markovify', 'MarkovChain')
-    def test_train_invalid_category(self, value):
-        """=> Ensure error is thrown if category does not exist."""
-        add_all()
-
-        with self.client:
-            response = self.client.post(
-                '/train',
-                data=json.dumps(dict(
-                    algo=value,
-                    category_id=99999999
-                    )),
-                content_type='application/json'
-            )
-            data = json.loads(response.data.decode())
-
-            self.assertEqual(response.status_code, 400)
-            self.assertIn('error', data['status'])
-            self.assertIn('Selected category does not exist.', data['message'])
-
-    @data('markovify', 'MarkovChain')
-    def test_train_no_corpus_text(self, value):
-        """=> Ensure error is thrown if there is no text in corpus."""
-        user = add_user('test', 'test@test.com', 'test')
-        cat = add_category('test', user.id)
-        cat = add_category('romans', user.id)  # to be sure no text exists
-
-        with self.client:
-            response = self.client.post(
-                '/train',
-                data=json.dumps(dict(
-                    algo=value,
-                    category_id=2
-                    )),
-                content_type='application/json'
-            )
-            data = json.loads(response.data.decode())
-
-            self.assertEqual(response.status_code, 400)
-            self.assertIn('fail', data['status'])
-            self.assertIn('Failed during model training. Please verify corpus texts.',
-                          data['message'])
 
     @data('markovify', 'MarkovChain')
     def test_generate_text(self, value):
@@ -166,14 +51,14 @@ class TestGeneratorService(BaseTestCase):
             self.assertIn('success', data['status'])
             self.assertNotEqual('', data['data']['text'])
 
-    @data('markovify', 'MarkovChain')
-    def test_generate_text_invalid_payload(self, value):
-        """=> Ensure error is thrown if no category and model are provided."""
+    @data('/train', '/generate')
+    def test_generator_invalid_payload(self, value):
+        """=> Ensure error is thrown if no category or model are provided."""
         add_all()
 
         with self.client:
             response = self.client.post(
-                '/generate',
+                value,
                 data=json.dumps(dict()),
                 content_type='application/json'
             )
@@ -183,16 +68,20 @@ class TestGeneratorService(BaseTestCase):
             self.assertIn('fail', data['status'])
             self.assertIn('Invalid payload.', data['message'])
 
-    @data('markovify', 'MarkovChain')
-    def test_generate_text_no_category(self, value):
+    @data(('markovify', '/train'),
+          ('markovify', '/generate'),
+          ('MarkovChain', '/train'),
+          ('MarkovChain', '/generate'))
+    @unpack
+    def test_generator_no_category(self, algo, route):
         """=> Ensure error is thrown if no category is provided."""
         add_all()
 
         with self.client:
             response = self.client.post(
-                '/generate',
+                route,
                 data=json.dumps(dict(
-                    algo=value
+                    algo=algo
                     )),
                 content_type='application/json'
             )
@@ -202,13 +91,14 @@ class TestGeneratorService(BaseTestCase):
             self.assertIn('fail', data['status'])
             self.assertIn('Invalid payload.', data['message'])
 
-    def test_generate_text_no_algo(self):
+    @data('/train', '/generate')
+    def test_generator_no_algo(self, value):
         """=> Ensure error is thrown if no model is provided."""
         add_all()
 
         with self.client:
             response = self.client.post(
-                '/generate',
+                value,
                 data=json.dumps(dict(
                     category_id=1
                     )),
@@ -220,13 +110,14 @@ class TestGeneratorService(BaseTestCase):
             self.assertIn('fail', data['status'])
             self.assertIn('Invalid payload.', data['message'])
 
-    def test_generate_text_invalid_model(self):
+    @data('/train', '/generate')
+    def test_generator_invalid_model(self, value):
         """=> Ensure error is thrown if model does not exist"""
         add_all()
 
         with self.client:
             response = self.client.post(
-                '/generate',
+                value,
                 data=json.dumps(dict(
                     algo='no-model',
                     category_id=1
@@ -239,16 +130,20 @@ class TestGeneratorService(BaseTestCase):
             self.assertIn('error', data['status'])
             self.assertIn('Selected algorithm does not exist.', data['message'])
 
-    @data('markovify', 'MarkovChain')
-    def test_generate_text_invalid_category(self, value):
+    @data(('markovify', '/train'),
+          ('markovify', '/generate'),
+          ('MarkovChain', '/train'),
+          ('MarkovChain', '/generate'))
+    @unpack
+    def test_generator_invalid_category(self, algo, route):
         """=> Ensure error is thrown if category does not exist."""
         add_all()
 
         with self.client:
             response = self.client.post(
-                '/generate',
+                route,
                 data=json.dumps(dict(
-                    algo=value,
+                    algo=algo,
                     category_id=99999999
                     )),
                 content_type='application/json'
@@ -259,18 +154,25 @@ class TestGeneratorService(BaseTestCase):
             self.assertIn('error', data['status'])
             self.assertIn('Selected category does not exist.', data['message'])
 
-    @data('markovify', 'MarkovChain')
-    def test_generate_text_no_corpus_text(self, value):
-        """=> Ensure error is thrown if there is no text in corpus."""
+    @data(('markovify', '/train'),
+          ('markovify', '/generate'),
+          ('MarkovChain', '/train'),
+          ('MarkovChain', '/generate'))
+    @unpack
+    def test_generator_no_file(self, algo, route):
+        """
+            => Ensure error is thrown if there is no text in corpus or
+               pre-trained models.
+        """
         user = add_user('test', 'test@test.com', 'test')
         cat = add_category('test', user.id)
-        cat = add_category('romans', user.id)  # to be sure no text exists
+        cat = add_category('romans', user.id)  # to be sure no file exist
 
         with self.client:
             response = self.client.post(
-                '/generate',
+                route,
                 data=json.dumps(dict(
-                    algo=value,
+                    algo=algo,
                     category_id=2
                     )),
                 content_type='application/json'
@@ -279,5 +181,10 @@ class TestGeneratorService(BaseTestCase):
 
             self.assertEqual(response.status_code, 400)
             self.assertIn('fail', data['status'])
-            self.assertIn('No result, maybe the model is not trained.',
-                          data['message'])
+
+            if route == '/train':
+                self.assertIn('Failed during model training. Please verify corpus texts.',
+                              data['message'])
+            else:
+                self.assertIn('No result, maybe the model is not trained.',
+                              data['message'])
