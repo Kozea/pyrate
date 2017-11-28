78b959e9591483eda81c4eb763e2faf01839739c
rojinpolat <rojin.polat69@gmail.com>
2017-11-28 17:43:17 +0100

    UpdateCategory( modifs) non fonctionnel

--- lib/frontend/src/components/CategoriesList.jsx
+++ lib/frontend/src/components/CategoriesList.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-import { deleteCategory, addCategory, updateCategory } from '../actions'
+import { deleteCategory, addCategory, editCat } from '../actions'
 
-function CategoriesList({ categories, btnClick, editClick, addCat }) {
+function CategoriesList({ categories, btnClick, addCat, showEdit, editClick }) {
   let input
   return (
     <div>
@@ -11,19 +11,29 @@ function CategoriesList({ categories, btnClick, editClick, addCat }) {
       <ul>
         {categories.map(category => (
           <li key={category.id}>
-            {category.label}
+            {showEdit ? (
+              <div>
+                <input value={category.label} />
+                <button type="submit">Valider</button>
+              </div>
+            ) : (
+              <div>
+                {category.label}
+                <button
+                  onClick={e => {
+                    e.preventDefault()
+                    btnClick(category.id)
+                  }}
+                >
+                  Supprimer
+                </button>
+              </div>
+            )}
+
             <button
               onClick={e => {
                 e.preventDefault()
-                btnClick(category.id)
-              }}
-            >
-              Supprimer
-            </button>
-            <button
-              onClick={e => {
-                e.preventDefault()
-                editClick(category.id)
+                editClick()
               }}
             >
               Modifer
@@ -51,13 +61,16 @@ function CategoriesList({ categories, btnClick, editClick, addCat }) {
 
 export default connect(
   function mapStateToProps(state) {
-    return { categories: state.categories }
+    return {
+      categories: state.categories,
+      showEdit: state.showEdit,
+    }
   },
   function mapDispatchToProps(dispatch) {
     return {
       btnClick: data => dispatch(deleteCategory(data)),
-      editClick: data => dispatch(updateCategory(data)),
       addCat: data => dispatch(addCategory(data)),
+      editClick: data => dispatch(editCat(data)),
     }
   }
 )(CategoriesList)
